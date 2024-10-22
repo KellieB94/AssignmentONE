d64c4d4ecf0734c6919e49a87c7d2137b51d290c
KellieB94 <sba24284@student.cct.ie>
2024-10-22 22:50:13 +0100

    Successfully read customers.txt file. Error found in one Customer Class

---
+++ src/ca1/CA1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca1;
+//PART ONE - READING FROM A FILE
+
+//Libraries listed to generate the customer file
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CA1 {
+
+    //https://github.com/KellieB94/CA1.git - GitHub Repo
+    
+        public static void main(String[] args) {
+       
+        //assignment of File Path to define the file
+        String custFile = "C:\\Users\\ismis\\customers.txt";
+        
+        //initialization of BufferedReader to load customer file
+        try (BufferedReader buf = new BufferedReader(new FileReader(custFile))){
+            String list;
+         while ((list = buf.readLine()) != null) {
+             System.out.println(list);
+         }
+        } catch (IOException k) {
+          k.printStackTrace();
+        }
+    }
+    
+}
+
